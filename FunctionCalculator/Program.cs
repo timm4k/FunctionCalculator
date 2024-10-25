2e0e10cc9012d408c6f83e8d5c1b916ca8783a92
Tanya <timmi.com>
2024-10-25 20:27:47 +0300

    Add project files.

---
+++ FunctionCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double x0 = 4.6;
+        double xk = 5.8;
+        double dx = 0.2;
+        double d = 1.3;
+        double y;
+
+        Console.WriteLine("Step\t x\t y");
+
+        double x = x0;
+        int step = 1;
+
+        while (x <= xk)
+        {
+            y = Math.Pow(x, 4) + Math.Cos(2 + Math.Pow(x, 3) - d);
+            Console.WriteLine($"{step}\t {x:F4}\t {y:F4}");
+            x += dx;
+            step++;
+        }
+    }
+}
